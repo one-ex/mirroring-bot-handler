0dfdf7f1ac098b4c4ee46b938964b7ef7077fd00
one-ex <exball649@gmail.com>
2026-02-19 22:43:46 +0800

    github/mirroring-bot-handler/bot.py

--- bot.py
+++ bot.py
@@ -138,7 +138,7 @@ def format_job_progress(job_info: dict, status_info: dict) -> dict:
         f"〚{bar}〛`{progress:.1f}%`\n"
         f"🚀 **Speed:** `{speed:.2f} MB/s`\n"
         f"⏳ **Estimation:** `{eta} Sec`\n"
-        f"🚫 /stop {job_id}"
+        f"🚫 **Cancel:** /stop_{job_id}"
     )
 
     # Keyboard is no longer used for active jobs
