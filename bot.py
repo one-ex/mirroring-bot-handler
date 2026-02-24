d2a938c867def96d1744956984b79b8e32a21168
one-ex <exball649@gmail.com>
2026-02-25 06:29:35 +0800

    github/mirroring-bot-handler/bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ if not WEBHOOK_HOST:
 GOFILE_API_URL = os.getenv('GOFILE_API_URL')
 PIXELDRAIN_API_URL = os.getenv('PIXELDRAIN_API_URL')
 AUTHORIZED_USER_IDS = [int(user_id) for user_id in os.getenv('AUTHORIZED_USER_IDS', '').split(',') if user_id]
-POLLING_INTERVAL = 1  # Detik
+POLLING_INTERVAL = 2  # Detik
 DATABASE_URL = os.getenv('DATABASE_URL')
 WEB_AUTH_URL = os.getenv('WEB_AUTH_URL')
 GDRIVE_API_URL = os.getenv('GDRIVE_API_URL')
