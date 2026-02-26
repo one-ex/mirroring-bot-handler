dfbb87b52eabbcaed4bed73782a5149aa4dfc11c
one-ex <exball649@gmail.com>
2026-02-26 23:53:29 +0800

    github/mirroring-bot-handler/start.sh

--- start.sh
+++ start.sh
@@ -9,3 +9,5 @@ echo "Starting application on port $PORT"
 
 # Jalankan dengan gunicorn dan uvicorn worker
 gunicorn -w 1 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT --timeout 60 bot:app
+
+curl -X GET https://web-auth-helper.onrender.com
