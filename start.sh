34e6decf2110df9a04d97c5da5a9c050834818d4
one-ex <exball649@gmail.com>
2026-02-27 09:28:42 +0800

    github/mirroring-bot-handler/start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
 PORT=${PORT:-10000}
 echo "Starting application on port $PORT"
-
-gunicorn -w 1 -k uvicorn.workers.UvicornWorker \
-  --bind 0.0.0.0:$PORT \
-  --timeout 60 \
-  --access-logfile - \
-  --error-logfile - \
-  --preload \
-  bot:app
+uvicorn bot:app --host 0.0.0.0 --port $PORT
